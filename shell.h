a785354d6c9e89abfc659a7af541b8a16641c16a
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-11-04 09:17:58 -0700

    l

--- shell.h
+++ shell.h
@@ -8,3 +8,26 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE
+#define HIST_MAX	4096
+
+extern char **environ;
