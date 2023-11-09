1ae9925f6fdf33d624f53b73357ee73a626bffaa
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-11-09 05:54:04 -0800

    l

--- shell.h
+++ shell.h
@@ -15,9 +15,10 @@
 #define MAX_ALIAS_NAME 100
 #define MAX_ALIAS_VALUE 100
 
-typedef struct {
-    char name[MAX_ALIAS_NAME];
-    char value[MAX_ALIAS_VALUE];
+typedef struct
+{
+	char name[MAX_ALIAS_NAME];
+	char value[MAX_ALIAS_VALUE];
 } Alias;
 
 
@@ -26,10 +27,11 @@ void handle_setenv(char **argv);
 void handle_unsetenv(char **argv);
 char *my_getline(int fd);
 char **split_line(char *line, int *argc, char separator);
-void execute_command(char **argv, char *envp[],Alias *aliasTable, int *aliasCount,int num_args);
-void addAlias(char* name, char* value,Alias *aliasTable,int *aliasCount);
-char* getAlias(char* name,Alias *aliasTable,int *aliasCount);
-void printAlias(Alias *aliasTable,int *aliasCount);
-char* replace_str(char* str, char* old_sub, char* new_sub);
-char* readFileContent(char *fileName);
+void execute_command(char **argv, char *envp[],
+		Alias *aliasTable, int *aliasCount, int num_args);
+void addAlias(char *name, char *value, Alias *aliasTable, int *aliasCount);
+char *getAlias(char *name, Alias *aliasTable, int *aliasCount);
+void printAlias(Alias *aliasTable, int *aliasCount);
+char *replace_str(char *str, char *old_sub, char *new_sub);
+char *readFileContent(char *fileName);
 #endif
