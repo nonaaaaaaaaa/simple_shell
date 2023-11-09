790291be45a1866a1297e778b39cae4adb348647
shehapkhalil62@gmail.com <shehapkhalil62@gamil.com>
2023-11-08 23:44:19 -0800

    betty

--- alias.c
+++ alias.c
@@ -1,24 +1,56 @@
 #include "shell.h"
 
-void addAlias(char* name, char* value,Alias *aliasTable,int *aliasCount) {
-    strcpy(aliasTable[*aliasCount].name, name);
-    strcpy(aliasTable[*aliasCount].value, value);
-    (*aliasCount)++;
+/**
+ * addAlias - add alias
+ * @name: name of alias
+ * @value: the value of alias
+ * @aliasTable: the table pf alias
+ * @aliasCount: the count
+ * Return: void
+ */
+void addAlias(char *name, char *value, Alias *aliasTable, int *aliasCount)
+{
+	strcpy(aliasTable[*aliasCount].name, name);
+	strcpy(aliasTable[*aliasCount].value, value);
+	(*aliasCount)++;
 }
 
-char* getAlias(char* name,Alias *aliasTable,int *aliasCount) {
-    	int i;
-	for(i = 0; i < *aliasCount; i++) {
-        	if(strcmp(aliasTable[i].name, name) == 0) {
-            		return aliasTable[i].value;
-        }
-    }
-    return NULL;
+
+/**
+ * getAlias - get the valie of alias
+ * @name: the name of alias
+ * @aliasTable:the array of alias
+ * @aliasCount:the count of alias
+ * Return: the vlaue of alias
+ */
+
+char *getAlias(char *name, Alias *aliasTable, int *aliasCount)
+{
+	int i;
+
+	for (i = 0; i < *aliasCount; i++)
+	{
+		if (strcmp(aliasTable[i].name, name) == 0)
+		{
+			return (aliasTable[i].value);
+		}
+	}
+	return (NULL);
 }
 
-void printAlias(Alias *aliasTable,int *aliasCount) {
-    int i;
-	for(i = 0; i < *aliasCount; i++) {
-        	printf("%s='%s'\n", aliasTable[i].name, aliasTable[i].value);
-    }
+/**
+ * printAlias - print the value of all alias
+ * @aliasTable: array of alias
+ * aliasCount:the count of alias
+ * Return: void
+ */
+
+void printAlias(Alias *aliasTable, int *aliasCount)
+{
+	int i;
+
+	for (i = 0; i < *aliasCount; i++)
+	{
+		printf("%s='%s'\n", aliasTable[i].name, aliasTable[i].value);
+	}
 }
--- file.c
+++ file.c
@@ -1,33 +1,36 @@
 #include "shell.h"
 
-char* readFileContent(char *fileName) {
-    FILE *file;
-    char *content;
-    size_t n = 0;
-    int c;
+/**
+ * readFileContent - read the last exit statue from file
+ * @fileName: filename
+ * Return:contant
+ */
 
-    file = fopen(fileName, "r");
-    if (file == NULL) {
+char *readFileContent(char *fileName)
+{
+	FILE *file;
+	char *content;
+	size_t n = 0;
+	int c;
+
+	file = fopen(fileName, "r");
+	if (file == NULL)
+	{
 	fclose(file);
-        return NULL;
-    }
-
-    content = malloc(1000);
-    if(content == NULL) {
-        printf("Cannot allocate memory \n");
-        return NULL;
-    }
-
-    
-    while ((c = fgetc(file)) != EOF)
-    {
-        content[n++] = (char)c;
-    }
-
-    
-    content[n] = '\0';
-    content[strcspn(content, "\n")] = '\0';
-
-    fclose(file);
-    return content;
+	return (NULL);
+	}
+	content = malloc(1000);
+	if (content == NULL)
+	{
+		printf("Cannot allocate memory \n");
+		return (NULL);
+	}
+	while ((c = fgetc(file)) != EOF)
+	{
+		content[n++] = (char)c;
+	}
+	content[n] = '\0';
+	content[strcspn(content, "\n")] = '\0';
+	fclose(file);
+	return (content);
 }
